1126fe1962bb00689e236f14f02ffb0ecbad6ef8
napthedev <phongna.dev@gmail.com>
2021-11-08 15:43:16 +0700

    fix logo size

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,9 +6,7 @@ const Navbar: NextPage = () => {
   return (
     <div className={`fixed top-0 left-0 right-0 w-screen py-5 px-7 h-20 z-50 flex justify-between items-center transition duration-500 bg-transparent navbar`}>
       <Link href="/">
-        <a>
-          <img className="h-full w-auto cursor-pointer" src="https://ik.imagekit.io/nap/eCinema/eCinema_Logo_X2vcmwMm4.png" alt="" />
-        </a>
+        <img className="h-full w-auto cursor-pointer" src="https://ik.imagekit.io/nap/eCinema/eCinema_Logo_X2vcmwMm4.png" alt="" />
       </Link>
       <Link href="/search">
         <a>
