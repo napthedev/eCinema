7a72f0e51e519b1372b1c81bcf61d0d76a247918
napthedev <phongna.dev@gmail.com>
2021-11-08 15:40:28 +0700

    fix pagination logic

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -14,61 +14,79 @@ const Pagination: NextPage<PaginationProps> = ({ current, maximum, resolveLink }
     <div className="flex items-center justify-center gap-1 flex-wrap">
       {current > 1 && (
         <Link href={resolveLink(current - 1)}>
-          <button className="rounded-btn">
-            <FaChevronLeft />
-          </button>
+          <a>
+            <button className="rounded-btn">
+              <FaChevronLeft />
+            </button>
+          </a>
         </Link>
       )}
       {current < 5 ? (
         <>
           {new Array(maximum < 5 ? maximum : 5).fill("").map((_, index) => (
             <Link key={index} href={resolveLink(index + 1)}>
-              <button className={`rounded-btn ${current === index + 1 ? "!bg-orange" : ""}`}>{index + 1}</button>
+              <a>
+                <button className={`rounded-btn ${current === index + 1 ? "!bg-orange" : ""}`}>{index + 1}</button>
+              </a>
             </Link>
           ))}
           {maximum > 5 && (
             <>
               <span>...</span>
               <Link href={resolveLink(maximum)}>
-                <button className="rounded-btn">{maximum}</button>
+                <a>
+                  <button className="rounded-btn">{maximum}</button>
+                </a>
               </Link>
             </>
           )}
         </>
-      ) : current > maximum - 5 ? (
+      ) : current > maximum - 4 ? (
         <>
           <Link href={resolveLink(1)}>
-            <button className="rounded-btn">1</button>
+            <a>
+              <button className="rounded-btn">1</button>
+            </a>
           </Link>
           <span>...</span>
           {new Array(5).fill("").map((_, index) => (
             <Link key={index} href={resolveLink(maximum - 4 + index)}>
-              <button className={`rounded-btn ${current === maximum - 4 + index ? "!bg-orange" : ""}`}>{maximum - 4 + index}</button>
+              <a>
+                <button className={`rounded-btn ${current === maximum - 4 + index ? "!bg-orange" : ""}`}>{maximum - 4 + index}</button>
+              </a>
             </Link>
           ))}
         </>
       ) : (
         <>
           <Link href={resolveLink(1)}>
-            <button className="rounded-btn">1</button>
+            <a>
+              <button className="rounded-btn">1</button>
+            </a>
           </Link>
           <span>...</span>
           {new Array(5).fill("").map((_, index) => (
-            <Link key={index} href={resolveLink(current - 5 + index)}>
-              <button className={`rounded-btn ${current === current - 5 + index ? "!bg-orange" : ""}`}>{current - 2 + index}</button>
+            <Link key={index} href={resolveLink(current - 2 + index)}>
+              <a>
+                <button className={`rounded-btn ${current === current - 2 + index ? "!bg-orange" : ""}`}>{current - 2 + index}</button>
+              </a>
             </Link>
           ))}
           <span>...</span>
           <Link href={resolveLink(maximum)}>
-            <button className="rounded-btn">{maximum}</button>
+            <a>
+              <button className="rounded-btn">{maximum}</button>
+            </a>
           </Link>
         </>
       )}
       {current < maximum && (
         <Link href={resolveLink(current + 1)}>
-          <button className="rounded-btn">
-            <FaChevronRight />
-          </button>
+          <a>
+            <button className="rounded-btn">
+              <FaChevronRight />
+            </button>
+          </a>
         </Link>
       )}
     </div>
