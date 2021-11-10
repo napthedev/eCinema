ad37af8996d4b56cd92d25a10936fdcdb606fd25
napthedev <phongna.dev@gmail.com>
2021-11-10 22:56:51 +0700

    responsive navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,13 +4,13 @@ import type { NextPage } from "next";
 
 const Navbar: NextPage = () => {
   return (
-    <div className={`fixed top-0 left-0 right-0 w-screen py-5 px-7 h-20 z-50 flex justify-between items-center transition duration-500 bg-transparent navbar`}>
+    <div style={{ minHeight: "60px" }} className="fixed top-0 left-0 right-0 w-screen py-3 md:px-7 px-3 h-[7vw] max-h-20 z-50 flex justify-between items-center transition duration-500 bg-transparent navbar">
       <Link href="/">
         <img className="h-full w-auto cursor-pointer" src="https://ik.imagekit.io/nap/eCinema/eCinema_Logo_X2vcmwMm4.png" alt="" />
       </Link>
       <Link href="/search">
         <a>
-          <FaSearch className="mr-4 cursor-pointer" size={30} />
+          <FaSearch className="mr-4 cursor-pointer" size={25} />
         </a>
       </Link>
     </div>
