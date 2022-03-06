ec0729fea09aab868e5f2fa4a27e52ba278d87d3
napthedev <phongna.dev@gmail.com>
2022-03-06 19:49:13 +0700

    Fix image cache bug

--- src/components/Shared/Image.tsx
+++ src/components/Shared/Image.tsx
@@ -1,31 +1,47 @@
-import { FC, HTMLProps, useState } from "react";
+import { FC, HTMLProps, useEffect, useRef, useState } from "react";
 
 interface ImageProps {
   opacity?: number;
+  src: string;
 }
 
 const Image: FC<HTMLProps<HTMLImageElement> & ImageProps> = ({
   style,
   crossOrigin: _,
   opacity = 1,
+  src,
   ...others
 }) => {
   const [loaded, setLoaded] = useState(false);
+  const [realSrc, setRealSrc] = useState("");
+
+  const imageRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    const handler = () => {
+      console.log("loaded");
+      setLoaded(true);
+    };
+
+    const current = imageRef.current;
+
+    current?.addEventListener("load", handler);
+
+    setRealSrc(src);
+
+    return () => current?.removeEventListener("load", handler);
+  }, [src]);
 
   return (
     <img
-      onLoadedData={() => {
-        setLoaded(true);
-      }}
-      onLoad={() => {
-        setLoaded(true);
-      }}
+      ref={imageRef}
       style={{
         ...style,
         transition: "0.3s",
         opacity: loaded ? opacity : 0,
       }}
       alt=""
+      src={realSrc}
       {...others}
     />
   );
