347e45fc1e34050f1d4bad61d295fbf2a1129567
napthedev <phongna.dev@gmail.com>
2022-07-03 17:26:41 +0700

    Chore: update 2embed url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,7 +4,6 @@ export const TMDB_IMAGE = "https://image.tmdb.org/t/p/";
 export const imageResize = (src: string, dimension: string = "w200") =>
   `${TMDB_IMAGE}${dimension}${src}`;
 export const imageOriginal = (src: string) => `${TMDB_IMAGE}original${src}`;
-export const embedMovie = (id: number) =>
-  `https://www.2embed.ru/embed/tmdb/movie?id=${id}`;
+export const embedMovie = (id: number) => `https://2embed.org/embed/${id}`;
 export const embedEpisode = (id: number, season: number, episode: number) =>
-  `https://www.2embed.ru/embed/tmdb/tv?id=${id}&s=${season}&e=${episode}`;
+  `https://2embed.org/embed/series?tmdb=${id}&sea=${season}&epi=${episode}`;
