654b4165ec22f16140b712c7f57d577a7c7adec4
Phong Nguyen <phongna.dev@gmail.com>
2024-02-17 15:03:19 +0700

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,6 +4,6 @@ export const TMDB_IMAGE = "https://image.tmdb.org/t/p/";
 export const imageResize = (src: string, dimension: string = "w200") =>
   `${TMDB_IMAGE}${dimension}${src}`;
 export const imageOriginal = (src: string) => `${TMDB_IMAGE}original${src}`;
-export const embedMovie = (id: number) => `https://2embed.org/embed/${id}`;
+export const embedMovie = (id: number) => `https://www.2embed.cc/embed/${id}`;
 export const embedEpisode = (id: number, season: number, episode: number) =>
-  `https://2embed.org/embed/series?tmdb=${id}&sea=${season}&epi=${episode}`;
+  `https://www.2embed.cc/embedtv/${id}&s=${season}&e=${episode}`;
